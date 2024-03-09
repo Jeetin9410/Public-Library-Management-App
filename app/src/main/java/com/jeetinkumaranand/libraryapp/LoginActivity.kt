2fef9f8e3f104aae6fcf7bfce427a801d7ad7370
Jeetin kumar anand <jeetin@flick2know.com>
2024-03-09 19:24:53 +0530

    feat(login): firebase login designed

---
+++ app/src/main/java/com/jeetinkumaranand/libraryapp/LoginActivity.kt
@@ -0,0 +1,79 @@
+package com.jeetinkumaranand.libraryapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.jeetinkumaranand.libraryapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var loginBinding: ActivityLoginBinding
+    //create an object from the FireBase off class in the global area
+    val auth:FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = loginBinding.root
+        setContentView(view)
+
+        loginBinding.buttonSignin.setOnClickListener {
+
+
+            val userEmail = loginBinding.editTextEmailSignin.text.toString()
+            val userPassword = loginBinding.editTextPasswordSignin.text.toString()
+
+
+            if (userEmail.isBlank() || userPassword.isBlank()){
+                Toast.makeText(applicationContext,"Enter the details ",Toast.LENGTH_SHORT).show()
+            }else {
+                //call
+                singinWithFirebase(userEmail, userPassword)
+            }
+
+        }
+        loginBinding.buttonSignup.setOnClickListener {
+
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginBinding.buttonForgot.setOnClickListener {
+            val intent = Intent(this, ForgetPasswordActivity::class.java)
+            startActivity(intent)
+        }
+        loginBinding.buttonSigninWithPhoneNumber.setOnClickListener {
+            val intent= Intent(this,PhoneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    fun singinWithFirebase(userEmail:String, userPassword: String) {
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext,"Login is successful",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    Toast.makeText(applicationContext,task?.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        if (user!= null){
+            val intent = Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/jeetinkumaranand/libraryapp/SignUpActivity.kt
@@ -0,0 +1,45 @@
+package com.jeetinkumaranand.libraryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.jeetinkumaranand.libraryapp.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    lateinit var signUpBinding: ActivitySignUpBinding
+
+    //create an object from the FireBase off class in the global area
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        signUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = signUpBinding.root
+        setContentView(view)
+
+        signUpBinding.buttonSignupUser.setOnClickListener {
+            val userEmail = signUpBinding.editTextEmailSignup.text.toString()
+            val userPassword = signUpBinding.editTextPasswordSignup.text.toString()
+
+            //call signupWithFirebase
+            signupWithFirebase(userEmail,userPassword)
+
+        }
+    }
+
+    fun signupWithFirebase(userEmail: String, userPassword: String) {
+
+        auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                Toast.makeText(applicationContext, "Your account has been created", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(applicationContext, task.exception?.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
