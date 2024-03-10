6f905d34cbce38a218e788ef3115b7404d3c113a
Jeetin kumar anand <jeetin@flick2know.com>
2024-03-10 22:56:48 +0530

    feat(ui): creating jetpack compose ui

--- app/src/main/java/com/jeetinkumaranand/libraryapp/LoginActivity.kt
+++ app/src/main/java/com/jeetinkumaranand/libraryapp/LoginActivity.kt
@@ -4,19 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jeetinkumaranand.libraryapp.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : ComponentActivity() {
 
-    lateinit var loginBinding: ActivityLoginBinding
+    //lateinit var loginBinding: ActivityLoginBinding
     //create an object from the FireBase off class in the global area
     val auth:FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContent {
+            LoginActivityUi()
+           // Text(text = "Jeetin")
+        }
+        /*loginBinding = ActivityLoginBinding.inflate(layoutInflater)
         val view = loginBinding.root
         setContentView(view)
 
@@ -49,6 +64,22 @@ class LoginActivity : AppCompatActivity() {
             val intent= Intent(this,PhoneActivity::class.java)
             startActivity(intent)
             finish()
+        }*/
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun LoginActivityUi(){
+        Surface(modifier = Modifier
+            .height(IntrinsicSize.Max)
+            .width(IntrinsicSize.Max)
+            .padding(20.dp),
+            color = Color(0xFFA1E2EB),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Column {
+                Text(text = "Jeetin")
+            }
         }
     }
     fun singinWithFirebase(userEmail:String, userPassword: String) {
@@ -76,4 +107,6 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
     }
+
+
 }
